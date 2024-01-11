8e5486f6006548d27a80f5a8e37abb2d621bce75
AndoniAlonsoTort <andoni.alonso-tort@etu.univ-lehavre.fr>
2024-01-12 00:25:10 +0100

    Add commentaires au contrat

--- contracts/DutchAuction.sol
+++ contracts/DutchAuction.sol
@@ -5,18 +5,16 @@ contract DutchAuction {
     address public auctioneer;
     uint public currentArticleIndex;
     uint public auctionStartTime;
-    uint public constant AUCTION_DURATION = 3600; // 1 hour
-    uint public constant STARTING_PRICE = 1 ether;
+    uint public constant AUCTION_DURATION = 3600; // Duration de l'enchere => 1 heure
+    uint public constant STARTING_PRICE = 1 ether; // On declare le prix de debut à 1 ether
     uint public constant PRICE_DECREMENT = 0.1 ether;
     uint public constant RESERVE_PRICE = 0.2 ether;
 
-    struct Article {
-        string name;
-        uint currentPrice;
-        address winningBidder;
-        bool closed;
-    }
+    /** Creation d'une sctructure pour l'article
+    Lequel aura un nom, un prix, un gagnant et un status pour savoir s'il est fermé  */
+    struct Article { string name; uint currentPrice; address winningBidder; bool closed; }
 
+    // Un tableau d'articles
     Article[] public articles;
 
     event BidPlaced(uint indexed articleIndex, address indexed bidder, uint amount);
@@ -41,7 +39,8 @@ contract DutchAuction {
     constructor() {
         auctioneer = msg.sender;
         auctionStartTime = block.timestamp;
-        // Add your articles to the auction
+
+        // Creation des articles à notre enchere
         articles.push(Article("Article 1", STARTING_PRICE, address(0), false));
         articles.push(Article("Article 2", STARTING_PRICE, address(0), false));
         currentArticleIndex = 0;
@@ -54,12 +53,15 @@ contract DutchAuction {
         return currentPrice > RESERVE_PRICE ? currentPrice : RESERVE_PRICE;
     }
 
+    /**
+        Placer une offre 
+     */
     function placeBid(uint articleIndex) external payable auctionOpen articleOpen(articleIndex) {
         require(msg.value > 0, "Le montant de l'enchere doit etre superieur a zero");
 
         uint currentPrice = getCurrentPrice();
         require(msg.value >= currentPrice, "Le montant de l'enchere est inferieur au prix actuel");
-
+        //  Si la valeur est > 0 et plus que le prix actuel on etabli le gagnant et on ferme l'article
         articles[articleIndex].winningBidder = msg.sender;
         articles[articleIndex].closed = true;
 
@@ -67,8 +69,10 @@ contract DutchAuction {
     }
 
     function moveToNextArticle() external onlyAuctioneer {
+        // Si l'index actuel est inferieux au total de notre collection, tous les articles on été vendus
         require(currentArticleIndex < articles.length - 1, "Tous les articles ont ete vendus aux encheres");
 
+        // Si on a encore des articles on augmente l'index et on passe au suivant
         currentArticleIndex++;
         articles[currentArticleIndex].currentPrice = getCurrentPrice();
         articles[currentArticleIndex].closed = false;
